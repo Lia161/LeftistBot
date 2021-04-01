2107ed9288a980a571f3c37b65c22d7464f8d2d0
lia_161 <lia.angerer@systemli.org>
2021-04-01 15:07:50 +0200

    verbessert

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ Bot to answer all leftist words in chat
 import telebot
 import datetime
 
-bot_token = "<TOKEN>"
+bot_token = "1703408507:AAHKvrd5l3Y94R0Gt3JoNgfKOrrne-kslVg"
 bot = telebot.TeleBot(bot_token)
 bot_name = '@antifaActionBot'
 
@@ -18,62 +18,36 @@ def send_start(message):
 
 @bot.message_handler(func=lambda message: True)
 def say_something(message):
-    text = message.text.split()
     chat = message.chat.id
-    for i in text:
-        i = i.lower()
-        if i[-1] == "." or i[-1] == "!" or i[-1] == "?" or i[-1] == " ":
-            i = i[:-2]
-        if i == "antifa":
-            bot.send_message(chat,"Antifaaaaaaaaaa <3")
-        elif i == "kommunismus":
-            bot.send_message(chat,"<3")
-        elif i == "anarchismus":
-            bot.send_message(chat,"<3")
-        elif i == "anarchie":
-            bot.send_message(chat,"<3")
-        elif i == "marx":
-            bot.send_message(chat,"Karl Marx <3")
-        elif i == "engels":
-            bot.send_message(chat,"<3")
-        elif i == "stalin":
-            bot.send_message(chat,"Verräter! 😤😡")
-        elif i == "manifest":
-            bot.send_message(chat,"<3")
-        elif i == "trotzki":
-            bot.send_message(chat,"<3")
-        elif i == "kropotkin":
-            bot.send_message(chat,"<3")
-        elif i == "bakunin":
-            bot.send_message(chat,"<3")
-        elif i == "rechts":
-            bot.send_message(chat,"Keinen Millimeter nach rechts!")
-        elif i == "links":
-            bot.send_message(chat,"Revolution?")
-        elif i == "spd" or i == "grüne":
-            bot.send_message(chat,"Wer hat uns verraten? 😤😡")
-        elif i == "il":
-            bot.send_message(chat,"<3")
-        elif i == "fascho" or i == "faschos":
-            bot.send_message(chat,"Nazis Töten. 🤬😡")
-        elif i == "nazi" or i == "nazis":
-            bot.send_message(chat, "Nazis Töten. 🤬😡")
-        elif i == "acab":
-            bot.send_message(chat,"HASS, HASS, HASS WIE NOCH NIE! ALL COPS ARE TARGETS ACAT 🤬😡")
-        elif i == "acat":
-            bot.send_message(chat, "HASS, HASS, HASS WIE NOCH NIE! ALL COPS ARE TARGETS ACAT 🤬😡")
-        elif i == "1312":
-            bot.send_message(chat, "HASS, HASS, HASS WIE NOCH NIE! ALL COPS ARE TARGETS ACAT 🤬😡")
-        elif i == "13120":
-            bot.send_message(chat, "HASS, HASS, HASS WIE NOCH NIE! ALL COPS ARE TARGETS ACAT 🤬😡")
-        elif i == "cops" or i == "cop":
-            bot.send_message(chat,"FCKCPS! 🤬😡")
-        elif i == "kapitalismus":
-            bot.send_message(chat,"Fight Capitalism!")
-        elif i == "lenin":
-            bot.send_message(chat,"Daddy <3")
-        elif i == "afd":
-            bot.send_message(chat,"FCKAFD!!")
+    text = message.text.lower()
+    if "antifa" in text:
+        bot.send_message(chat,"Antifaaaaaaaaaa <3")
+    elif "left unity" in text or "kommunismus" in text or "luxemburg" in text or "anarchismus" in text or "anarchie" in text or "engels" in text or "manifest" in text or "trotzki" in text or "kropotkin" in text or "bakunin" in text or "il" in text or "pkk" in text or "kpd" in text:
+        bot.send_message(chat, "<3")
+    elif "kapitalismus" in text or "capitalism" in text:
+        bot.send_message(chat,"Fight Capitalism!")
+    elif "acab" in text or "acat" in text or "1312" in text:
+        bot.send_message(chat,"HASS, HASS, HASS WIE NOCH NIE! ALL COPS ARE TARGETS ACAT 🤬😡")
+    elif "cop" in text:
+        bot.send_message(chat,"FCKCPS! 🤬😡")
+    elif "afd" in text:
+        bot.send_message(chat,"FCKAFD!!")
+    elif "lenin" in text:
+        bot.send_message(chat,"Daddy <3")
+    elif "stalin" in text:
+        bot.send_message(chat,"Verräter! 😤😡")
+    elif "nazi" in text or "fascho" in text:
+        bot.send_message(chat, "Nazis Töten. 🤬😡")
+    elif "spd" in text or "grüne" in text:
+        bot.send_message(chat,"Wer hat uns verraten? 😤😡")
+    elif "links" in text:
+        bot.send_message(chat,"Revolution?")
+    elif "rechts" in text:
+        bot.send_message(chat,"Keinen Millimeter nach rechts!")
+    elif "marx" in text:
+        bot.send_message(chat,"Karl Marx <3")
+    elif "revolution" in text:
+        bot.send_message(chat,"What solution? Reeeevoluution ✊")
 
 while True:
     try:
